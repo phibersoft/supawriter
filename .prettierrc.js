d5164380e9ed6acbb5129ffc837c917fc8162e34
phibersoft <spirnet@hotmail.com>
2024-04-14 06:21:23 +0300

    config: extend importOrder of prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-    printWidth: 130,
-    tabWidth: 2,
-    plugins: ["@trivago/prettier-plugin-sort-imports"],
-    importOrder: ["^\\./.*\\.scss$", "<THIRD_PARTY_MODULES>", "^@/assets", "^@/", "^@/*.scss", "^[./]"],
-    importOrderSeparation: true,
+  printWidth: 130,
+  tabWidth: 2,
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrder: [
+    "^\\./.*\\.scss$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/assets",
+    "^@/app",
+    "^@/components",
+    "^@/services",
+    "^@/util",
+    "^@/",
+    "^@/*.scss",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
 };
