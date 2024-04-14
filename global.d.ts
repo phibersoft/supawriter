a645e06d9abc6894c8900aa350d44ff5beba9098
phibersoft <spirnet@hotmail.com>
2024-04-14 06:21:45 +0300

    type-safety: process.env keys

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,11 @@
 export declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            PARAGRAPH_API_URL: string;
+            SUPABASE_SERVICE_ROLE_KEY: string;
+            NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+            NEXT_PUBLIC_SUPABASE_URL: string;
+            NEXT_PUBLIC_PARAGRAPH_API_URL: string;
+            NEXT_PUBLIC_PROJECT_LINK: string;
         }
     }
 }
