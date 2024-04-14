71bb1e8085a728924b64559294f8c2dffa269908
phibersoft <spirnet@hotmail.com>
2024-04-14 10:52:28 +0300

    feat: paragraphApi.getRandomWords implementation

--- services/paragraph-api.ts
+++ services/paragraph-api.ts
@@ -1,7 +1,9 @@
 const paragraphApi = {
-  getRandomParagraph: async () => {
-    const response = await fetch(process.env.NEXT_PUBLIC_PARAGRAPH_API_URL);
-    return await response.text();
+  getRandomWords: async (wordCount = 85): Promise<string[]> => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_PARAGRAPH_API_URL}?words=${wordCount}`, {
+      cache: "no-cache",
+    });
+    return await response.json();
   },
 };
 
