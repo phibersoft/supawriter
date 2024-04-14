08569757c2452b39f87641c3fe53c630436d5d6e
phibersoft <spirnet@hotmail.com>
2024-04-14 11:25:31 +0300

    feat: default word count is relative to WRITER_DURATION

--- services/paragraph-api.ts
+++ services/paragraph-api.ts
@@ -1,5 +1,7 @@
+import { WRITER_DURATION } from "@/CONSTANTS";
+
 const paragraphApi = {
-  getRandomWords: async (wordCount = 85): Promise<string[]> => {
+  getRandomWords: async (wordCount = WRITER_DURATION + 10): Promise<string[]> => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_PARAGRAPH_API_URL}?words=${wordCount}`, {
       cache: "no-cache",
     });
