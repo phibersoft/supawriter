ee3c3577d49ae2f56fca98c3f6eaec96e93845b5
phibersoft <spirnet@hotmail.com>
2024-04-14 20:53:35 +0300

    refac: Nickname not setting local storage internally anymore

--- components/Nickname.tsx
+++ components/Nickname.tsx
@@ -2,18 +2,21 @@
 
 import { FC, useState } from "react";
 
-import { globalKeyboard } from "@/components/Keyboard";
 import { Input } from "@/components/shared";
 
-import { useFakeTyper, useLocalStorage } from "@/hooks";
+import { useFakeTyper } from "@/hooks";
 
-const Nickname: FC = () => {
+type NicknameProps = {
+  onNicknameSet: (nickname: string) => void;
+};
+
+const Nickname: FC<NicknameProps> = ({ onNicknameSet }) => {
   const placeholder = useFakeTyper(["Barry Allen", "Usain Bolt", "Sonic", "Quicksilver", "The Flash"]);
-  const [nickname, setNickname] = useLocalStorage("nickname", "");
+  const [nickname, setNickname] = useState<string>("");
   const [focused, setFocused] = useState(false);
 
   return (
-    <div className={"flex flex-col items-center"}>
+    <div className={"flex flex-col items-center p-5"}>
       <label htmlFor={"nickname"} className={"text-md font-bold md:text-lg lg:text-2xl"}>
         How do slow people address you?
       </label>
@@ -22,6 +25,11 @@ const Nickname: FC = () => {
         type={"text"}
         value={nickname}
         onChange={(e) => setNickname(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onNicknameSet(nickname);
+          }
+        }}
         className={`shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#dc2626,0_0_15px_#dc2626,0_0_30px_#dc2626]`}
         placeholder={focused ? "" : placeholder}
         onFocus={() => setFocused(true)}
