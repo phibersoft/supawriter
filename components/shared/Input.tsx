d4895e31a21ca124289822c8e6678b1c2fecd828
phibersoft <spirnet@hotmail.com>
2024-04-14 07:26:09 +0300

    feat: common Input component implementation

---
+++ components/shared/Input.tsx
@@ -0,0 +1,53 @@
+import { FC, InputHTMLAttributes } from "react";
+
+import { globalKeyboard } from "@/components/Keyboard";
+
+type InputProps = {} & InputHTMLAttributes<HTMLInputElement>;
+
+export const Input: FC<InputProps> = ({ onFocus, onBlur, onKeyDown, className = "", ...props }) => {
+  const _onFocus: typeof onFocus = (event) => {
+    globalKeyboard.stopAnimation();
+
+    if (onFocus) {
+      onFocus(event);
+    }
+  };
+
+  const _onBlur: typeof onBlur = (event) => {
+    globalKeyboard.startAnimation();
+
+    if (onBlur) {
+      onBlur(event);
+    }
+  };
+
+  const _onKeyDown: typeof onKeyDown = (event) => {
+    globalKeyboard.highlightKey(event.key);
+
+    if (onKeyDown) {
+      onKeyDown(event);
+    }
+  };
+
+  return (
+    <input
+      type={"text"}
+      className={`
+                bg-transparent
+                border border-gray-300 rounded-md 
+                max-w-[500px] w-full
+                px-1 py-2 md:px-2 md:py-3 lg:px-3 lg:py-4
+                m-3 lg:m-5
+                text-center text-md text-white
+                focus:outline-none
+                placeholder-gray-600
+                disabled:opacity-50
+                ${className}
+            `}
+      onFocus={_onFocus}
+      onBlur={_onBlur}
+      onKeyDown={_onKeyDown}
+      {...props}
+    />
+  );
+};
--- components/shared/index.tsx
+++ components/shared/index.tsx
@@ -1,3 +1,4 @@
 export * from "./Button";
 export * from "./Tooltip";
 export * from "./Title";
+export * from "./Input";
