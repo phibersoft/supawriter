816ead60fd1d454e001475322ebad013aeb5ec2e
phibersoft <spirnet@hotmail.com>
2024-04-14 07:29:49 +0300

    refac: Nickname uses shared Input

--- components/Nickname.tsx
+++ components/Nickname.tsx
@@ -3,6 +3,7 @@
 import { FC, useState } from "react";
 
 import { globalKeyboard } from "@/components/Keyboard";
+import { Input } from "@/components/shared";
 
 import { useFakeTyper, useLocalStorage } from "@/hooks";
 
@@ -16,23 +17,16 @@ const Nickname: FC = () => {
       <label htmlFor={"nickname"} className={"text-md font-bold md:text-lg lg:text-2xl"}>
         How do slow people address you?
       </label>
-      <input
+      <Input
         id={"nickname"}
         type={"text"}
         value={nickname}
         onChange={(e) => setNickname(e.target.value)}
-        onKeyDown={(e) => {
-          globalKeyboard.highlightKey(e.key);
-        }}
-        className={`bg-transparent border border-gray-300 rounded-md 
-        px-3 py-5 m-2 w-full max-w-[500px]
-        text-center text-md text-white focus:outline-none
-        placeholder-gray-600 
-        shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#dc2626,0_0_15px_#dc2626,0_0_30px_#dc2626]
-        lg:p-2 lg:m-4 lg:text-lg`}
+        className={`shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#dc2626,0_0_15px_#dc2626,0_0_30px_#dc2626]`}
         placeholder={focused ? "" : placeholder}
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
+        key={`nickname-input`}
       />
     </div>
   );
